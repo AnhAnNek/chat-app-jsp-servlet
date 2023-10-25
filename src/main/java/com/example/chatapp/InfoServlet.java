d8b5f51a85d65ea3c7e93d95b87539941a34b9db
AnhAnNek <info.vananne@gmail.com>
2023-10-25 08:59:28 +0700

    completed

---
+++ src/main/java/com/example/chatapp/InfoServlet.java
@@ -0,0 +1,21 @@
+package com.example.chatapp;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/info")
+public class InfoServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+
+        req.setAttribute("name", name);
+
+        String url = "/index.jsp";
+        getServletContext().getRequestDispatcher(url).forward(req, resp);
+    }
+}
